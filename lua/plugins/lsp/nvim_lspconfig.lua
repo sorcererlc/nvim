ffa537a7029b68d55f8e508a045b066f573720c9
Cristian Toma <cristi@cristiant.com>
2025-03-11 13:02:33 +0200

    Update mason config.

--- lua/plugins/lsp/nvim_lspconfig.lua
+++ lua/plugins/lsp/nvim_lspconfig.lua
@@ -201,7 +201,6 @@ return {
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
     require('mason-lspconfig').setup {
-      ensure_installed = true,
       automatic_installation = true,
       handlers = {
         function(server_name)
