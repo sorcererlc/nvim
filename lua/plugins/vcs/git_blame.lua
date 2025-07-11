396bdcf6eea616aecd3517f1ef8a67a5618c4130
Cristian Toma <cristi@cristiant.com>
2025-07-11 16:29:18 +0300

    Fix git blame template.

--- lua/plugins/vcs/git_blame.lua
+++ lua/plugins/vcs/git_blame.lua
@@ -10,7 +10,7 @@ return {
     -- your configuration comes here
     -- for example
     enabled = true, -- if you want to enable the plugin
-    message_template = ' <summary> • author> • <date> • <<<sha>>', -- template for the blame message, check the Message template section for more options
+    message_template = ' <summary> • <author> • <date> • <<<sha>>', -- template for the blame message, check the Message template section for more options
     date_format = '%d-%m-%Y %H:%M:%S', -- template for the date, check Date format section for more options
     virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
   },
